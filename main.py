4ce85d874d6fb16b91d8713fc89a412a5d51be61
skripas1973-coder <skripas1973@gmail.com>
2026-04-30 17:39:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import telebot
+import os
 
-TOKEN = "ТУТ_БУДЕТ_ТВОЙ_ТОКЕН"
+# Берём токен из Railway
+TOKEN = os.getenv("TOKEN")
+
+# Проверка (чтобы не падал молча)
+if TOKEN is None:
+    print("❌ TOKEN не найден!")
+    exit()
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Бот работает 🚀")
+
+print("Бот запущен...")
 
 bot.infinity_polling()
