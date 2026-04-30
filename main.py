3344aea8adc0586706cb17b7f794366e75377481
skripas1973-coder <skripas1973@gmail.com>
2026-04-30 17:47:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import os
 # Берём токен из Railway
 TOKEN = os.getenv("TOKEN")
 
-# Проверка (чтобы не падал молча)
+# Проверка
 if TOKEN is None:
     print("❌ TOKEN не найден!")
     exit()
 
 bot = telebot.TeleBot(TOKEN)
 
+# Команда /start
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Бот работает 🚀")
+
+# Ответ на любые сообщения
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, "Ты написал: " + message.text)
 
 print("Бот запущен...")
 
